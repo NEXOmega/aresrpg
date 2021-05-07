21c60cf3e5d9342bb2a12be35cd9788bb04246e7
Duarte David <deltaduartedavid@gmail.com>
2021-05-07 22:07:02 +0200

    Update for cairo

--- flake.nix
+++ flake.nix
@@ -5,28 +5,36 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
+    flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        
+
         inputLock = (builtins.fromJSON (builtins.readFile ./package-lock.json));
 
-        rewriteDep = package: package //
-          (if package ? resolved then {
-            resolved = "file://${pkgs.fetchurl {
-              url = package.resolved;
-              outputHashAlgo = builtins.elemAt (builtins.split "-" package.integrity) 0;
-              outputHash = builtins.elemAt (builtins.split "-" package.integrity) 2;
-            }}";
-          } else {}) //
-          (if package ? dependencies then {
-            dependencies = rewriteDeps package.dependencies;
-          } else {});
+        rewriteDep = package:
+          package // (if package ? resolved then {
+            resolved = "file://${
+                pkgs.fetchurl {
+                  url = package.resolved;
+                  outputHashAlgo =
+                    builtins.elemAt (builtins.split "-" package.integrity) 0;
+                  outputHash =
+                    builtins.elemAt (builtins.split "-" package.integrity) 2;
+                }
+              }";
+          } else
+            { }) // (if package ? dependencies then {
+              dependencies = rewriteDeps package.dependencies;
+            } else
+              { });
 
-        rewriteDeps = dependencies: builtins.mapAttrs (name: rewriteDep) dependencies;
+        rewriteDeps = dependencies:
+          builtins.mapAttrs (name: rewriteDep) dependencies;
 
-        outputLock = builtins.toJSON ({ lockfileVersion = 2; dependencies = rewriteDeps inputLock.dependencies; });
+        outputLock = builtins.toJSON ({
+          lockfileVersion = 2;
+          dependencies = rewriteDeps inputLock.dependencies;
+        });
 
         # TODO: remove when flakes support submodules
         floor1 = pkgs.fetchFromGitHub {
@@ -36,10 +44,27 @@
           sha256 = "WuKsDHpWmgJlqNCQ9Z5V75P5AF1w3cOIDbuMxQ/EGaw=";
         };
 
+        nodeSources = pkgs.runCommand "node-sources" { } ''
+          tar --no-same-owner --no-same-permissions -xf ${pkgs.nodejs-15_x.src}
+          mv node-* $out
+        '';
+
+        cairo = pkgs.cairo.override {
+          x11Support = false;
+          gobjectSupport = false;
+          pdfSupport = false;
+        };
+
+        pango = pkgs.pango.override {
+          cairo = cairo;
+          x11Support = false;
+        };
+
         aresrpg = pkgs.stdenv.mkDerivation {
           name = "aresrpg";
 
-          nativeBuildInputs = [ pkgs.nodejs-15_x ];
+          nativeBuildInputs =
+            [ pkgs.nodejs-15_x cairo pango pkgs.python3 pkgs.pkg-config ];
 
           src = ./.;
 
@@ -60,39 +85,37 @@
             rm -rf world/floor1
             ln -sT ${floor1} world/floor1
 
-            npm ci --production
+            npm ci --production --build-from-source --nodedir=${nodeSources}
 
             rm package-lock.json node_modules/.package-lock.json
           '';
         };
-      in
-        {
-          devShell = pkgs.mkShell {
-            buildInputs = [
-              pkgs.nodejs-15_x
-              pkgs.libuuid
-              pkgs.cairo
-              pkgs.pango
-              pkgs.libjpeg
-              pkgs.giflib
-              pkgs.librsvg
-              pkgs.python3
-              pkgs.pkg-config
-            ];
-          };
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            pkgs.nodejs-15_x
+            pkgs.libuuid
+            pkgs.cairo
+            pkgs.pango
+            pkgs.libjpeg
+            pkgs.giflib
+            pkgs.librsvg
+            pkgs.python3
+            pkgs.pkg-config
+          ];
+        };
 
-          dockerImage = pkgs.dockerTools.buildImage {
-            name = "aresrpg";
-            tag = "latest";
+        dockerImage = pkgs.dockerTools.buildImage {
+          name = "aresrpg";
+          tag = "latest";
 
-            config = {
-              Cmd = [ "${pkgs.nodejs-slim-15_x}/bin/node" "${aresrpg}/src/index.js" ];
-              Label = nixpkgs.lib.mkIf (self ? rev) "rev=${self.rev}";
-              ExposedPorts = {
-                "25565/tcp" = {};
-              };
-            };
+          config = {
+            Cmd =
+              [ "${pkgs.nodejs-slim-15_x}/bin/node" "${aresrpg}/src/index.js" ];
+            ExposedPorts = {
+              "25565/tcp" = { };
+            } // (if (self ? rev) then { Label = "rev=${self.rev}"; } else { });
           };
-        }
-      );
+        };
+      });
 }
